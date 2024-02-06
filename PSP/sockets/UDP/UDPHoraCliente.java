4fbb6ae654333e5eb77fd010f46c99fc4b67c808
morgul27 <joseminanca2000@gmail.com>
2024-02-06 12:53:04 +0100

    soquets

--- PSP/sockets/UDP/UDPHoraCliente.java
+++ PSP/sockets/UDP/UDPHoraCliente.java
@@ -1,51 +1,53 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class UDPHoraCliente {
     private final static int MAX_BYTES = 1400;
     private final static String COD_TEXTO = "UTF-8";
     private final static String servidorIP = "localhost";
     private static int puerto = 9999;
+    private static int timeout = 5000;
 
     public static void main(String[] args) {
 
-        try (var serverSocket = new DatagramSocket(puerto)) { // crea el socket con el número de puerto
-            System.out.printf("Creado socket de datagrama para puerto %s.\n", puerto);
+        try (DatagramSocket serverSocket = new DatagramSocket();
+                InputStreamReader isrStdIn = new InputStreamReader(System.in, COD_TEXTO);
+                BufferedReader brStdIn = new BufferedReader(isrStdIn)) {
 
-            while (true) {
+            serverSocket.setSoTimeout(timeout);
+            String lineaLeida;
+            System.out.println("Introducir palabra para petición al servidor.");
+            System.out.print("Linea>");
 
-                System.out.println("Esperando datagramas ...");
-                // creamos array de bytes para almacenar mensaje recibido
-                byte[] datosRecibidos = new byte[MAX_BYTES];
-                // creamos datagrama usando el buffer
-                DatagramPacket paqueteRecibido = new DatagramPacket(datosRecibidos, datosRecibidos.length);
+            while ((lineaLeida = brStdIn.readLine()) != null && lineaLeida.length() > 0) {
 
-                // recepcion del datagrama
-                serverSocket.receive(paqueteRecibido);
+                // Obtenemos la dirección del servidor
+                InetAddress IPServidor = InetAddress.getByName(servidorIP);
+                byte[] b = lineaLeida.getBytes(COD_TEXTO);
+                DatagramPacket paqueteEnviado = new DatagramPacket(b, b.length, IPServidor, puerto);
 
-                // codificamos el texto en UTF-8
-                int lineaRecibida = Integer
-                        .parseInt(new String(paqueteRecibido.getData(), 0, paqueteRecibido.getLength(), COD_TEXTO));
-
-                // obtenemos la IP y puerto del cliente que está contenida en el datagrama
-                InetAddress IPCliente = paqueteRecibido.getAddress();
-                int puertoCliente = paqueteRecibido.getPort();
-
-                System.out.println();
-
-                System.out.printf("\n", IPCliente.getHostAddress(),
-                        puertoCliente,
-                        lineaRecibida * lineaRecibida);
-                String respuesta = "#" + lineaRecibida;
-
-                byte[] b = respuesta.getBytes(COD_TEXTO);
-                DatagramPacket paqueteEnviado = new DatagramPacket(b, b.length, IPCliente, puertoCliente);
+                serverSocket.connect(IPServidor, puerto);
                 serverSocket.send(paqueteEnviado);
-            }
 
+                byte[] respuesta = new byte[MAX_BYTES];
+                DatagramPacket respuestaPacket = new DatagramPacket(respuesta, respuesta.length);
+
+                try {
+                    serverSocket.receive(respuestaPacket);
+                    String fecha = new String(respuestaPacket.getData(), 0, respuestaPacket.getLength(), COD_TEXTO);
+                    System.out.println("El dia y la hora de hoy es: " + fecha);
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Error: Tiempo de espera agotado. No se recibió respuesta del servidor.");
+                }
+                System.out.println(
+                        "Si desea ver otra vez la fecha envia un caracter, si desea terminar envia el mensaje vacio");
+            }
         } catch (SocketException ex) {
             System.out.println("Excepción de sockets");
             ex.printStackTrace();
--- PSP/sockets/UDP/UDPHoraServidor.java
+++ PSP/sockets/UDP/UDPHoraServidor.java
@@ -3,16 +3,18 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UDPHoraServidor {
-    private final static int MAX_BYTES = 1400;
+	private final static int MAX_BYTES = 1400;
 	private final static String COD_TEXTO = "UTF-8";
 	private static int puerto = 9999;
 
 	public static void main(String[] args) {
 
-		try (var serverSocket = new DatagramSocket(puerto)) { // crea el socket con el número de puerto
+		try (var serverSocket = new DatagramSocket(puerto)) {
 			System.out.printf("Creado socket de datagrama para puerto %s.\n", puerto);
 
 			while (true) {
@@ -21,32 +23,32 @@ public class UDPHoraServidor {
 				// creamos array de bytes para almacenar mensaje recibido
 				byte[] datosRecibidos = new byte[MAX_BYTES];
 				// creamos datagrama usando el buffer
-				DatagramPacket paqueteRecibido = new DatagramPacket(datosRecibidos, datosRecibidos.length); // crea un
-																											// paquete
-																											// para
-																											// recibir
-																											// los datos
+				DatagramPacket paqueteRecibido = new DatagramPacket(datosRecibidos, datosRecibidos.length);
 				// recepcion del datagrama
 				serverSocket.receive(paqueteRecibido);
 
 				// codificamos el texto en UTF-8
-				int lineaRecibida = Integer
-						.parseInt(new String(paqueteRecibido.getData(), 0, paqueteRecibido.getLength(), COD_TEXTO));
+				String lineaRecibida = (new String(paqueteRecibido.getData(), 0, paqueteRecibido.getLength(),
+						COD_TEXTO));
 
 				// obtenemos la IP y puerto del cliente que está contenida en el datagrama
 				InetAddress IPCliente = paqueteRecibido.getAddress();
 				int puertoCliente = paqueteRecibido.getPort();
 
-				System.out.println();
+				DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				Date date = new Date();
+				String respuesta = dateFormat.format(date);
 
 				System.out.printf("\n", IPCliente.getHostAddress(),
 						puertoCliente,
-						lineaRecibida * lineaRecibida);
-				String respuesta = "#" + lineaRecibida;
+						lineaRecibida);
 
-				byte[] b = respuesta.getBytes(COD_TEXTO);
-				DatagramPacket paqueteEnviado = new DatagramPacket(b, b.length, IPCliente, puertoCliente);
+			
+				byte[] responseData = respuesta.getBytes(COD_TEXTO);
+				DatagramPacket paqueteEnviado = new DatagramPacket(responseData, responseData.length, IPCliente, puertoCliente);
 				serverSocket.send(paqueteEnviado);
+			
+
 			}
 
 		} catch (SocketException ex) {
